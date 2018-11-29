8b3cb150505dff419a93491e3e173de14b537d62
SaleteTeixeira <salete.diasteixeira@hotmail.com>
2018-11-29 00:13:13 +0000

    adicionados close

---
+++ trabalho2/word_change/build/lib/word_change/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from .createdb import create
+from .createdb import generator
+from .createdb import printdb
+
+from .parser import creator
+from .parser import replace_latex
+from .parser import spotlight_latex
+from .parser import replace
+from .parser import spotlight
+
+from .Word import Word
