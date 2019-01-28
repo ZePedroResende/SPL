dbe491b0a3ff4e3d6cfb2b314cdbe6d1f3bc432a
SaleteTeixeira <salete.diasteixeira@hotmail.com>
2019-01-28 15:03:31 +0000

    alteradas funçoes para serem inseridas num modulo

--- trabalho3/remove_spaces.py
+++ trabalho3/remove_spaces.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import re
+import sys
 
-def remove_espacos(filename):
+def remove_espacosAux(filename, outname):
     text = open(filename, 'r')
-    outname = re.sub(r'(.+\/)*(.+)', r'\1spaceless_\2', filename)
     outfile = open(outname, 'w')
 
     for line in text:
@@ -12,3 +12,11 @@ def remove_espacos(filename):
             
     text.close()
     outfile.close()
+
+def remove_espacos():
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+        outname = re.sub(r'(.+\/)*(.+)', r'\1spaceless_\2', filename)
+        remove_espacosAux(filename, outname)
+    else:
+        print('número de argumentos inválido')
